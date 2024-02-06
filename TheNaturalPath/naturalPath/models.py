93325cada78af924538e65797575a95a6907a0d1
Ricky <56303584+runningindos@users.noreply.github.com>
2024-02-06 23:41:02 +0000

    added more functionality to the database tables, created providers table and set code for the appearance of providers from menu. Also updated 'find NDs' to 'Meet Your New Provider' which I think is better

--- TheNaturalPath/naturalPath/models.py
+++ TheNaturalPath/naturalPath/models.py
@@ -7,7 +7,11 @@ class Providers(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
-    
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
 class Users(models.Model):
     uid = models.IntegerField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- TheNaturalPath/naturalPath/views.py
+++ TheNaturalPath/naturalPath/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render, HttpResponse    
+from django.shortcuts import render
+from django.http import HttpResponse 
+from .models import Providers, Users, Practice
 
 # Create your views here.
 
-def home(request):
+def home(request, context):
+
+    context={
+        "Providers" : Providers.objects.all()
+    }
     return render(request, 'home.html')
 
     
