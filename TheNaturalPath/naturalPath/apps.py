b698243d154814f8aedada61fd55f182668f58cb
Ricky <56303584+runningindos@users.noreply.github.com>
2024-02-06 21:48:45 +0000

    Created app and database classes. Working on connecting to Database today

---
+++ TheNaturalPath/naturalPath/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NaturalpathConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'naturalPath'
---
+++ TheNaturalPath/naturalPath/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Providers(models.Model):
+    prid = models.NumberField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+    
+class Users(models.Model):
+    uid = models.NumberField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+
+class Practice(models.Model):
+    pracid = models.NumberField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    prac_phone = models.CharField(max_length=200)
+    prac_email = models.CharField(max_length=200)
+    prac_name = models.CharField(max_length=200)
+    prac_address = models.CharField(max_length=200)
+    prac_city = models.CharField(max_length=50)
+    prac_region = models.CharField(max_length=100)
+    prac_country = models.CharField(max_length=100)
