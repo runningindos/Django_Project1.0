051ad384b986312183e9a5aed0cbf067aa6a1cf4
Ricky <56303584+runningindos@users.noreply.github.com>
2024-02-09 00:21:13 +0000

    got the templates to work finally, created paths and routes, and imported necessary files.

--- TheNaturalPath/naturalPath/urls.py
+++ TheNaturalPath/naturalPath/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from . import views
+from .import views
+
+
 
 urlpatterns = [
-    path("", views.home, name="home")
-]
+    path("", views.home, name="np-home"), 
+    path("providers/", views.providers, name="providers"),
+    path("register/", views.register, name="register"), 
+    path("login/", views.login, name="login"), 
+    path("logout/", views.logout, name="logout"), 
+    path("success/", views.success, name="success"), 
+  ]
+
+
+
+
+
+
--- TheNaturalPath/naturalPath/views.py
+++ TheNaturalPath/naturalPath/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 from .models import Providers, Users, Practice
+from django.shortcuts import render
 
 # Create your views here.
 
 def home(request, ):
     return render(request, 'home.html')
 
-    
+def providers(request, ):
+    context = {
 
+        'providers' : providers
+    }
+    return render(request, 'providers.html', context)
+ 
+def register(request, ):
+    return render(request,'register.html')
+
+def login(request, ):
+    return render(request, 'login.html')
+
+def logout(request, ):
+    return render(request, 'logout.html')
+
+def success(request, ):
+    return render(request,'success.html')
+
+
+Providers = [
+    {
+        'first_name': 'Caoidhreach',
+        'last_name': 'Mac hAidhearais',
+        'email': '<ricky@ricky.com>',
+    },
+    {
+        'first_name': 'George',
+        'last_name': 'Mahony',
+        'email': '<george@george.com>',
+    },
+    {
+        'first_name': 'Jane',
+        'last_name': 'Doe',
+        'email': '<jane@jane.com>',
+    }
+]
